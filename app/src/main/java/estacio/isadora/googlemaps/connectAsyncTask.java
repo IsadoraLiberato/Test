d8f744d354b13736b9dfcbbd2044eb74ed825c25
Isadora <isaliberatoisa@gmail.com>
2018-03-20 20:41:12 -0300

    AplicativoTest

---
+++ app/src/main/java/estacio/isadora/googlemaps/connectAsyncTask.java
@@ -0,0 +1,13 @@
+package estacio.isadora.googlemaps;
+
+/**
+ * Created by isado on 15/03/2018.
+ */
+
+class connectAsyncTask {
+    public connectAsyncTask(String urlTopass) {
+    }
+
+    public void execute() {
+    }
+}
